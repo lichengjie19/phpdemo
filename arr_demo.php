a81bf736f502ccb4184f4b4078c4b749a1c008cd
licjd <licjd@jiadaibao.com>
2020-03-06 17:53:29 +0800

    测试 array_key_exists("123", null)

--- arr_demo.php
+++ arr_demo.php
@@ -11,5 +11,29 @@ $membersIds = "";
 $membersIdsArr = explode(",", $membersIds);
 //var_dump($membersIdsArr);
 
-if (empty([])) echo '$a 为空' . "";
-if (!empty([1, 2])) echo '$a 不为空' . "";
+//if (empty([])) echo '$a 为空' . "";
+//if (!empty([1, 2])) echo '$a 不为空' . "";
+
+
+
+$friendResultList = [
+//    "isNotRelationList" => [
+////        ["memberID" => "652218035093049947", "updateTime" => "2015-01-01 00:00:00"]
+//    ],
+];
+
+$friendIDList = "[\"644663181288753369\"]";
+var_dump($friendIDList);
+
+echo "<br />";
+var_dump(array_key_exists("123", null));
+
+if (array_key_exists("123", null)) {
+    echo "<br />";
+    echo "if";
+} else {
+    echo "<br />";
+    echo "else";
+}
+
+
